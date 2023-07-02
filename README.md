3387cb03d02bfd59971321f33a707460ed41eac8
anton-bregolas <106439112+anton-bregolas@users.noreply.github.com>
2023-07-03 02:00:14 +0300

    SF Lab 03: Slider with slideshow and autoplay button completed.

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 SF Lab 03: Slider
 ==================================================================
-## Main branch
-
 TO DO:
 
-- Управляющие элементы, которые вам нужно установить и которые являются условиями проекта:
+- Get rid of innerHTML += str to follow best practices.
+- Mobile version of the slider.
+- Simple version of the slider.
 
-- Стрелки (переключают слайды по кольцу. То есть после слайда №3 снова пойдет слайд №1);
-- Кружочки между стрелками (включают нужный слайд);
-- Ссылки сверху слайдера (включают нужный слайд).
+## DONE
+
+***index_slideshow.html***
+
+- Slideshow version of the page with customizable autoplay.
+- As the page is loaded, the JS script inserts images, titles and descriptions from an array of Projects.
+- Navigation toggles between the images and sets of texts using opacity.
+- Autoplay can be switched on and off using the play button above the images.
+- JS file includes the following autoplay options: 
+> *pause:* Whether or not the slideshow is active when page loaded (true by default)
+> *playInterval:* How fast the slides are changing (in milliseconds)
+
+## TASK
+
+- Navigation via arrows. Slides must loop back to no.1 after no.3.
+- Dots between the arrows must select the corresponding slide.
+- Links above the slider must toggle between the slides.
--- js/slider_slideshow.js
+++ js/slider_slideshow.js
@@ -1,67 +1,54 @@
 let projects = [{
   url: "./images/projects-admiral.png",
   title: "Rostov-on-Don, Admiral",
-  city: "Rostov-on-Don<br> LCD admiral",
+  city: "Rostov-on-Don <br>LCD admiral",
   area: "81 m2",
   time: "3.5 months",
   cost: "Upon request"
 }, {
   url: "./images/projects-thieves.png",
   title: "Sochi Thieves",
-  city: "Sochi<br> Thieves",
+  city: "Sochi <br>Thieves",
   area: "105 m2",
   time: "4 months",
   cost: "Upon request"
 }, {
   url: "./images/projects-patriotic.png",
   title: "Rostov-on-Don Patriotic",
-  city: "Rostov-on-Don<br> Patriotic",
+  city: "Rostov-on-Don <br>Patriotic",
   area: "93 m2",
   time: "3 months",
   cost: "Upon request"
 }];
 
-function initSlider() {
-  // if (!projects || !projects.length) return;
+function initSlider(options) {
 
-  // options = options || {
-  //   titles: false,
-  //   dots: true,
-  //   autoplay: true
-  // };
+  let pause = options.pause;
+  let interval = options.autoplayInterval;
+  let playInterval = null;
 
-  let sliderImages = document.querySelector(".projects-img-box");
-  let sliderArrows = document.querySelector(".projects-arrow-nav");
-  let sliderDots = document.querySelector(".projects-bullets");
+  const sliderWrapper = document.querySelector(".projects");
 
-  // const sliderWrapper = document.querySelector(".projects");
+  let sliderImages = sliderWrapper.querySelector(".projects-img-box");
+  let sliderArrows = sliderWrapper.querySelector(".projects-arrow-nav");
+  let sliderDots = sliderWrapper.querySelector(".projects-bullets");
+  let sliderTitles = sliderWrapper.querySelector(".projects-nav-title");
 
-  // let sliderImages = sliderWrapper.querySelector(".projects-img-box");
-  // let sliderArrows = sliderWrapper.querySelector(".projects-arrow");
-  // let sliderDots = sliderWrapper.querySelector(".projects-bullet");
-  // let sliderTitle = sliderWrapper.querySelector(".projects-nav-item");
-
-  // let detailsCity = sliderWrapper.querySelector("#city");
-  // let detailsArea = sliderWrapper.querySelector("#area");
-  // let detailsTime = sliderWrapper.querySelector("#time");
-  // let detailsCost = sliderWrapper.querySelector("#cost");
+  let detailsCity = sliderWrapper.querySelector("#city");
+  let detailsArea = sliderWrapper.querySelector("#area");
+  let detailsTime = sliderWrapper.querySelector("#time");
+  let detailsCost = sliderWrapper.querySelector("#cost");
 
   initImages();
   initArrows();
-  initAutoplay();
   initDots();
+  initTitles();
+  initDetails();
+  createAutoButton();
 
-  // if (options.dots) {
-  // initDots();
-  // }
-  
-  // if (options.titles) {
-  //   initTitles();
-  // }
-  
-  // if (options.autoplay) {
-  //   initAutoplay();
-  // }
+  if (!pause) {
+    playInterval = setInterval(initAutoplay, interval);
+  }
 
   function initImages() {
     projects.forEach((image, index) => {
@@ -89,7 +76,8 @@ function initSlider() {
 
   function initDots() {
     projects.forEach((project, index) => {
-      let dot = `<div class="projects-bullet n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
+      let dot = `<div class="projects-bullet n${index} ${index === 0? "active" : ""}" 
+      data-index="${index}"></div>`;
       sliderDots.innerHTML += dot;
     });
     sliderDots.querySelectorAll(".projects-bullet").forEach(dot => {
@@ -98,40 +86,96 @@ function initSlider() {
       })
     })
   }
-  
+
+  function initTitles() {
+    projects.forEach((project, index) => {
+      let navTitle = `<li class="title projects-nav-item">
+      <a class="nav-item-title n${index} ${index === 0? "active" : ""}" 
+      data-index="${index}">${project.title}</a></li>`;
+      sliderTitles.innerHTML += navTitle;
+    });
+    sliderTitles.querySelectorAll(".nav-item-title").forEach(title => {
+      title.addEventListener("click", function() {
+        moveSlider(this.dataset.index);
+      })
+    })
+  }
+
+  function initDetails() {
+
+    projects.forEach((project, index) => {
+
+      let initCity = `<p class="projects-subtext text-p 
+      n${index} ${index === 0? "active" : ""}" 
+      data-index="${index}">${project.city}</p>`;
+
+      let initArea = `<p class="projects-subtext text-p 
+      n${index} ${index === 0? "active" : ""}" 
+      data-index="${index}">${project.area}</p>`;
+
+      let initTime = `<p class="projects-subtext text-p 
+      n${index} ${index === 0? "active" : ""}" 
+      data-index="${index}">${project.time}</p>`;
+
+      let initCost = `<p class="projects-subtext text-p 
+      n${index} ${index === 0? "active" : ""}" 
+      data-index="${index}">${project.cost}</p>`;
+
+      detailsCity.innerHTML += initCity;
+      detailsArea.innerHTML += initArea;
+      detailsTime.innerHTML += initTime;
+      detailsCost.innerHTML += initCost;
+    });
+  }
+
   function moveSlider(num) {
     sliderImages.querySelector(".active").classList.remove("active");
     sliderImages.querySelector(".n" + num).classList.add("active");
-    // if (options.dots) {
     sliderDots.querySelector(".active").classList.remove("active");
     sliderDots.querySelector(".n" + num).classList.add("active");
-    // }
-    // if (options.titles) changeTitle(num);
-  }
-  
-  function initAutoplay() {
-    setInterval(() => {
-      let curNumber = +sliderImages.querySelector(".active").dataset.index;
-      let nextNumber = curNumber === projects.length - 1? 0 : curNumber + 1;
-      moveSlider(nextNumber);
-    }, 5000);
-    
-    // options.autoplayInterval);
+    sliderTitles.querySelector(".active").classList.remove("active");
+    sliderTitles.querySelector(".n" + num).classList.add("active");
+
+    detailsCity.querySelector(".active").classList.remove("active");
+    detailsCity.querySelector(".n" + num).classList.add("active");
+    detailsArea.querySelector(".active").classList.remove("active");
+    detailsArea.querySelector(".n" + num).classList.add("active");
+    detailsTime.querySelector(".active").classList.remove("active");
+    detailsTime.querySelector(".n" + num).classList.add("active");
+    detailsCost.querySelector(".active").classList.remove("active");
+    detailsCost.querySelector(".n" + num).classList.add("active");
   }
 
+  function createAutoButton() {
+    let navButton = `<li class="nav-item-button-play ${pause === false? "active" : ""}">&#9658;</li>`;
+    sliderTitles.insertAdjacentHTML("beforeend", navButton);
+    sliderTitles.querySelector(".nav-item-button-play").addEventListener("click", toggleAutoplay);
+  }
+
+  function initAutoplay() {
+      let curNumber = +sliderImages.querySelector(".active").dataset.index;
+      let nextNumber = curNumber === projects.length - 1 ? 0 : curNumber + 1;
+      moveSlider(nextNumber);
+  }
+
+  function toggleAutoplay() {
+    if (pause === false) {
+      pause = true;
+      clearInterval(playInterval);
+      sliderTitles.querySelector(".nav-item-button-play").classList.remove("active");
+    } else {
+      pause = false;
+      sliderTitles.querySelector(".nav-item-button-play").classList.add("active");
+      playInterval = setInterval(initAutoplay, interval);
+    }
+  }
 }
 
-// let sliderOptions = {
-//   dots: true,
-//   titles: false,
-//   autoplay: true,
-//   autoplayInterval: 5000
-// };
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   initSlider(sliderOptions);
-// });
+let sliderOptions = {
+  pause: false,
+  autoplayInterval: 2000
+};
 
 document.addEventListener("DOMContentLoaded", function() {
-  initSlider();
+  initSlider(sliderOptions);
 });
